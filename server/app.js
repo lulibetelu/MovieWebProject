68c85dbe6623af676a093080bb617e8a9d3401e1
luciabetelu <lubetelu@gmail.com>
2025-11-11 09:29:17 -0300

    despues de cambiar todas las queries de la busqueda peliculas anda bien!

--- server/app.js
+++ server/app.js
@@ -115,7 +115,7 @@ app.get(API_URL + "/buscar", async (req, res) => {
     const offset = (page - 1) * 20;
 
     // Los placeholders en pg son $1, $2, etc.
-    const query = `
+    const queryDef = `
       SELECT * 
       FROM (
         SELECT * FROM search_all($1, 1000000)
@@ -123,35 +123,92 @@ app.get(API_URL + "/buscar", async (req, res) => {
         SELECT keyw.movie_id, keyw.title, 'movie'::TEXT AS type 
         FROM search_movies_by_keyword($2) AS keyw
       ) AS combined
-      LIMIT $3 OFFSET $4;
+    OFFSET $3
+      ;
     ` // ILIKE es case-insensitive en Postgres
-    const values = [`%${searchTerm}%`, searchTerm, limit, offset];
+    const values = [`%${searchTerm}%`, searchTerm, offset];
+
+    const queryMovies =`
+        (
+            SELECT
+               m.movie_id,
+               m.title
+            FROM Movies.movie m
+            WHERE m.title ILIKE $1
+            ORDER BY m.popularity DESC NULLS LAST
+        )
+        UNION ALL
+        SELECT keyw.movie_id, keyw.title
+        FROM movies.search_movies_by_keyword($4) AS keyw
+        LIMIT $2 OFFSET $3;
+    `
+    const movieValues = [`%${searchTerm}%`, limit, offset, searchTerm];
+
+    const queryActors =
+        `SELECT
+            p.person_id,
+            p.person_name
+        FROM person p
+        JOIN movie_cast mc ON mc.person_id = p.person_id
+        WHERE p.person_name ILIKE $1
+        GROUP BY p.person_id, p.person_name
+        LIMIT $2 OFFSET $3;`
+
+    const actorValues = [`%${searchTerm}%`, limit, offset];
+
+    const queryDirectors =
+        `SELECT
+            p.person_id,
+            p.person_name
+        FROM person p
+        JOIN movie_crew mc ON mc.person_id = p.person_id
+        JOIN department d ON d.department_id = mc.department_id
+        WHERE p.person_name ILIKE $1
+          AND mc.job = 'Director'
+        GROUP BY p.person_id, p.person_name
+        ORDER BY p.person_name
+        LIMIT $2 OFFSET $3;`
+    const directoresValues = [`%${searchTerm}%`, limit, offset];
 
     try {
         // Usar db.query que devuelve una promesa y acceder a .rows
-        const result = await db.query(query, values);
+        const pelis = (await db.query(queryMovies, movieValues)).rows;
+        console.log('ACA: ' + pelis.length);
+        const actores = (await db.query(queryActors, actorValues)).rows;
 
-        // Filter movies
-        const filteredMovies = result.rows.filter(
-            (row) => row.type === "movie",
-        );
+        const dir = (await db.query(queryDirectors, directoresValues)).rows;
 
-        // Filter actors
-        const filteredActors = result.rows.filter(
-            (row) => row.type === "actor",
-        );
+        const searchInfo = {
+            movies: [],
+            actors: [],
+            directors: [],
+        }
 
-        // Filter directors
-        const filteredDirectors = result.rows.filter(
-            (row) => row.type === "director",
-        );
+        pelis.forEach((pelicula) => {
+            console.log("PELICULA: " + pelicula.title);
+            searchInfo.movies.push({
+                name: pelicula.title,
+                movie_id: pelicula.movie_id,
+            });
+        });
 
+        actores.forEach((actor) => {
+            searchInfo.actors.push({
+                actor_id: actor.person_id,
+                name: actor.person_name,
+            });
+        });
+
+        dir.forEach((director) => {
+            searchInfo.directors.push({
+                director_id: director.person_id,
+                name: director.person_name,
+            })
+        })
 
         if (API_MODE) {
             res.json({
-                movies: filteredMovies,
-                actors: filteredActors,
-                directors: filteredDirectors,
+                searchInfo,
                 tmdbApiKey: process.env.TMDB_API_KEY,
                 searchTerm,
             });
